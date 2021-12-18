0bfb73d542957b71fb9e1c4bd28d6ae4eae6637e
Quinten <quinten.roets@gmail.com>
2021-12-18 16:45:58 +0100

    make files sync more simple

--- sysetup/starter.py
+++ sysetup/starter.py
@@ -9,18 +9,21 @@ from . import installer, git, desktopfiles, files
 
 
 def setup():
-    if "pw" not in os.environ:
-        # download and apply core config files
-        Backup.download(Path.home(), "Config", filters=["+ /.*"])
-        Cli.run("source ~/.bash_profile; sysetup")
-
+    Cli.run(f"drive pull {path_name}" for path_name in ["config", "docs", "browser"])
+    
+    if "pw" in os.environ:
+        setup_root()
     else:
-        files.setup() # files needed to know what to install
-        installer.install()
-        git.setup()
+        # load new environ
+        Cli.run("source ~/.bash_profile", "sysetup")
+    
+def setup_root():
+    files.setup() # files needed to know what to install
+    installer.install()
+    git.setup()
 
-        if Gui.ask_yn("Ready for reboot?"):
-            Cli.run("sudo reboot now")
+    if Gui.ask_yn("Ready for reboot?"):
+        Cli.run("sudo reboot now")
 
 if __name__ == "__main__":
     setup()
