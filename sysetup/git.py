47cd32ff3bc0c05878449aba1d97bddfddec353c
Quinten <quinten.roets@gmail.com>
2021-12-29 21:49:39 +0100

    add progress bar

--- sysetup/git.py
+++ sysetup/git.py
@@ -1,7 +1,9 @@
 from github import Github
 import os
+from tqdm import tqdm
 
 from libs.cli import Cli
+from libs.threading import Threads
 from .path import Path
 
 def setup():
@@ -9,19 +11,23 @@ def setup():
         os.environ["gittoken"]
         )
     user = g.get_user()
-
-    for repo in user.get_repos():
-        collabs = repo.get_collaborators()
-        if collabs.totalCount == 1 and collabs[0].login == user.login and not repo.archived:
-            name = repo.name.lower()
-            if not list(Path.scripts.rglob(f"{name}/.git")):
-                url = add_password(repo.clone_url)
-                Cli.run(f"git clone {url} {Path.scripts / name}")
-                if (path / "setup.py").exists():
-                    Cli.run(f"pip3 install -e {path}")
-    
-    for setup in Path.scripts.rglob("setup.py"):
+    repos = user.get_repos()
+    progress = tqdm("Checking repos")
+    Threads(check_repo, repos, user=user, progress=progress).join()
+    for setup in tqdm(Path.scripts.rglob("setup.py"), "Installing repos locally"):
         Cli.run(f"pip3 install --force-reinstall --no-deps -e {setup.parent}")
+        
+
+def check_repo(repo, user, progress): 
+    collabs = repo.get_collaborators()
+    if collabs.totalCount == 1 and collabs[0].login == user.login and not repo.archived:
+        name = repo.name.lower()
+        if not list(Path.scripts.rglob(f"{name}/.git")):
+            url = add_password(repo.clone_url)
+            Cli.get(f"git clone {url} {Path.scripts / name}")
+            if (path / "setup.py").exists():
+                Cli.get(f"pip3 install -e {path}")
+    progress.update()
 
 
 def add_password(url):
