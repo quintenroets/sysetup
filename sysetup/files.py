0a9a77872df628611785fe2758d90f38c3997c41
Quinten <quinten.roets@gmail.com>
2022-03-26 15:05:42 +0100

    fix typos

--- sysetup/files.py
+++ sysetup/files.py
@@ -6,38 +6,44 @@ from .path import Path
 
 
 def setup():
-    Backup().download('- /.config/browser/*', '+ /.config/scripts/backup/exports/.*', '- /.config/scripts/backup/exports/*', '/.**', quiet=False)
+    Backup().download(
+        "- /.config/browser/*",
+        "+ /.config/scripts/backup/exports/.*",
+        "- /.config/scripts/backup/exports/*",
+        "/.**",
+        quiet=False,
+    )
     BackupManager.after_pull()
-    
-    move_files(Path.assets / 'root')
-    move_files(Path.assets / 'home', Path.HOME)
+
+    move_files(Path.assets / "root")
+    move_files(Path.assets / "home", Path.HOME)
     move_crontab()
     # trust_keyboard()
     # seems to work without this command for now
 
 
-def move_files(src_root, dst_root=Path('/')):
-    for src in src_root.rglob('*'):
+def move_files(src_root, dst_root=Path("/")):
+    for src in src_root.rglob("*"):
         if src.is_file():
             dst = dst_root / src.relative_to(src_root)
             commands = (
-                [f'unzip -o '{src}' -d '{dst.parent}'']
-                if src.suffix == '.zip'
-                else [f'mkdir -p '{dst.parent}'', f'cp -f '{src}' '{dst}'']
-                )
-            
+                [f"unzip -o '{src}' -d '{dst.parent}'"]
+                if src.suffix == ".zip"
+                else [f"mkdir -p '{dst.parent}'", f"cp -f '{src}' '{dst}'"]
+            )
+
             cli.run_commands(*commands, root=dst.is_root, capture_output=True)
 
 
 def move_crontab():
-    src = Path.assets / 'crontab' / 'crontab'
-    cli.run('crontab -', input=src.text)
-    
+    src = Path.assets / "crontab" / "crontab"
+    cli.run("crontab -", input=src.text)
+
 
 def trust_keyboard():
-    keyboard = cli.get('bluetoothctl list | grep Keyboard', shell=True)
-    cli.run('bluetoothctl trust', keyboard, wait=False) # blocks if not found
+    keyboard = cli.get("bluetoothctl list | grep Keyboard", shell=True)
+    cli.run("bluetoothctl trust", keyboard, wait=False)  # blocks if not found
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup()
