244f5ca394e7a9aa62e9146c0e2add4f0bf12b53
Quinten <quinten.roets@gmail.com>
2022-03-26 15:09:01 +0100

    fix typos

--- sysetup/git.py
+++ sysetup/git.py
@@ -10,36 +10,35 @@ from .path import Path
 
 
 def setup():
-    g = Github(
-        os.environ['gittoken']
-        )
+    g = Github(os.environ["gittoken"])
     user = g.get_user()
     repos = list(user.get_repos())
-    progress = tqdm(repos, desc='Cloning repos')
+    progress = tqdm(repos, desc="Cloning repos")
     Threads(check_repo, repos, user=user, progress=progress).join()
-    
-    local_repos = list(Path.scripts.rglob('setup.py'))
-    for setup in tqdm(local_repos, 'Reinstalling editable repos'):
-        cli.get('pip3 install --force-reinstall --no-deps -e', setup.parent)
-    if Path.scripts.exists():
-        (Path.scripts / 'assets').symlink_to(Path.script_assets)
-        
 
-def check_repo(repo, user, progress): 
+    local_repos = list(Path.scripts.rglob("setup.py"))
+    for setup in tqdm(local_repos, "Reinstalling editable repos"):
+        cli.get("pip3 install --force-reinstall --no-deps -e", setup.parent)
+    if Path.scripts.exists():
+        (Path.scripts / "assets").symlink_to(Path.script_assets)
+
+
+def check_repo(repo, user, progress):
     collabs = repo.get_collaborators()
     if collabs.totalCount == 1 and collabs[0].login == user.login and not repo.archived:
         name = repo.name.lower()
-        if not list(Path.scripts.rglob(f'{name}/.git')):
+        if not list(Path.scripts.rglob(f"{name}/.git")):
             url = add_password(repo.clone_url)
             path = Path.scripts / name
-            cli.get('git clone', url, path)
-            if (path / 'setup.py').exists():
-                cli.get('pip3 install -e', path)
+            cli.get("git clone", url, path)
+            if (path / "setup.py").exists():
+                cli.get("pip3 install -e", path)
     progress.update()
 
 
 def add_password(url):
-    return url.replace('https://', f'https://{os.environ['gittoken']}@')
+    return url.replace("https://", f"https://{os.environ['gittoken']}@")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     setup()
