3f6f55adef77f98587d934e4aba62f19ebdee8c1
Quinten <quinten.roets@gmail.com>
2023-09-29 16:21:45 +0200

    remove obsolete copy

--- sysetup/files.py
+++ sysetup/files.py
@@ -12,23 +12,9 @@ def setup():
     for kwargs in kwargs_list:
         backup.Backup(quiet=False, confirm=False, **kwargs).pull()
 
-    move_files(Path.assets / "home", Path.HOME)
     move_crontab()
     # trust_keyboard()
     # seems to work without this command for now
-
-
-def move_files(src_root, dst_root=Path("/")):
-    for src in src_root.rglob("*"):
-        if src.is_file():
-            dst = dst_root / src.relative_to(src_root)
-            commands = (
-                [f"unzip -o '{src}' -d '{dst.parent}'"]
-                if src.suffix == ".zip"
-                else [f"mkdir -p '{dst.parent}'", f"cp -f '{src}' '{dst}'"]
-            )
-
-            cli.run_commands(*commands, root=dst.is_root, capture_output=True)
 
 
 def move_crontab():
