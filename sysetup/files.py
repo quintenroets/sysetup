f0b1f83842633ceb153f008b18cf4ca0bc72d208
Quinten <quinten.roets@gmail.com>
2023-10-14 07:26:32 -0700

    add setup file moving

--- sysetup/files.py
+++ sysetup/files.py
@@ -1,5 +1,6 @@
 import cli
 from backup.backups import Backup
+from backup.backups.cache import raw
 from backup.utils import Path as BackupPath
 
 from .path import Path
@@ -12,14 +13,16 @@ def setup():
         "environment": dict(
             filter_rules=["+ /etc/environment", "- *"], sync_remote=False
         ),
+        "setup files": dict(
+            sub_check_path=BackupPath.script_assets / "sysetup" / "files"
+        ),
     }
     for name, kwargs in kwargs_mapper.items():
         print(f"Downloading {name}..")
         Backup(quiet=False, confirm=False, **kwargs).pull()
 
     move_crontab()
-    # trust_keyboard()
-    # seems to work without this command for now
+    move_setup_files()
 
 
 def move_crontab():
@@ -27,9 +30,22 @@ def move_crontab():
     cli.run("crontab -", input=src.text)
 
 
-def trust_keyboard():
-    keyboard = cli.get("bluetoothctl devices | grep Keyboard", shell=True)
-    cli.run("bluetoothctl trust", keyboard, wait=False)  # blocks if not found
+def move_setup_files():
+    setup_files_root = BackupPath.script_assets / "sysetup" / "files"
+    setup_files = []
+    archived_setup_files = []
+    for path in setup_files_root.rglob("*"):
+        if path.is_file():
+            files = setup_files if path.archive_format is None else archived_setup_files
+            files.append(path)
+
+    backup = raw.Backup(paths=setup_files)
+    backup.pull()
+
+    for path in archived_setup_files:
+        dest = (backup.source / path.relative_to(setup_files_root)).parent
+        with cli.status(f"Unpacking {path.relative_to(setup_files_root)}"):
+            cli.get("unzip -o", path, "-d", dest, root=dest.is_root)
 
 
 if __name__ == "__main__":
