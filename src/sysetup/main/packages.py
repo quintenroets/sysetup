d9f63d8f09a8b5bda2326e1af593f746f48d1295
Quinten <quinten.roets@gmail.com>
2025-03-18 19:18:14 -0700

    move install to sysetup

--- src/sysetup/main/packages.py
+++ src/sysetup/main/packages.py
@@ -1,3 +1,8 @@
+import platform
+import shlex
+import warnings
+from collections.abc import Iterable
+
 import cli
 
 from sysetup.context import context
@@ -19,11 +24,14 @@ def enable_sudo() -> None:
 
 def install_packages() -> None:
     download_directory(Path.packages)
-    installations = {"packages": None, "snap": "snap install"}
+    packages_install_command = (
+        "apt-get install -y" if context.apt_is_installed else "pacman -S --noconfirm"
+    )
+    installations = {"packages": packages_install_command, "snap": "snap install"}
     for name, command in installations.items():
         path = (Path.packages / name).with_suffix(".yaml")
         packages: list[str] = path.yaml
-        cli.install(*packages, install_command=command)
+        install(packages, install_command=command)
 
     if not context.apt_is_installed:
         commands = "sudo pacman -S --noconfirm base-devel", "uv pip install wheel"
@@ -61,3 +69,15 @@ def update_apt() -> None:
     cli.run_commands_in_shell(*commands)
     if not Path("/snap").exists():
         cli.run("ln -s /var/lib/snapd/snap /snap", root=True)
+
+
+def install(packages: Iterable[str], install_command: str) -> None:
+    is_linux = platform.system() == "Linux"
+    if not is_linux:
+        message = "Required packages can only be installed on Linux"
+        warnings.warn(message, stacklevel=2)
+        return
+
+    for package in packages:
+        args = shlex.split(package)
+        cli.run(install_command, *args, root=True, check=False)
