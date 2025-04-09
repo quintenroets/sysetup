82e08a2118c7583a1625ececd38f16b3de64d2d4
Quinten <quinten.roets@gmail.com>
2025-04-09 03:54:25 +0000

    pre-commit auto updates

--- src/sysetup/models/path.py
+++ src/sysetup/models/path.py
@@ -2,6 +2,7 @@ from typing import TypeVar, cast
 
 import superpathlib
 from simple_classproperty import classproperty
+from typing_extensions import Self
 
 T = TypeVar("T", bound="Path")
 
@@ -9,19 +10,20 @@ T = TypeVar("T", bound="Path")
 class Path(superpathlib.Path):
     @classmethod
     @classproperty
-    def source_root(cls: type[T]) -> T:
+    def source_root(cls) -> Self:
         return cls(__file__).parent.parent
 
     @classmethod
     @classproperty
-    def assets(cls: type[T]) -> T:
+    def assets(cls) -> Self:
         path = cls.script_assets / cls.source_root.name
-        return cast(T, path)
+        return cast("Self", path)
 
     @classmethod
     @classproperty
-    def config(cls: type[T]) -> T:
+    def config(cls) -> Self:
         path = cls.assets / "config" / "config.yaml"
+<<<<<<< HEAD
         return cast(T, path)
 
     @classmethod
@@ -41,3 +43,6 @@ class Path(superpathlib.Path):
     def update_wallpaper_script(cls: type[T]) -> T:
         path = cls.source_root / "assets" / "scripts" / "update_wallpaper.js"
         return cast(T, path)
+=======
+        return cast("Self", path)
+>>>>>>> template
