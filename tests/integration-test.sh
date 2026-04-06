d8c71fd3fb9108f7dbbbdc1f89564ce2a9c48371
Quinten <quinten.roets@gmail.com>
2026-04-05 19:10:16 -0700

    unify e2e tests

--- tests/integration-test.sh
+++ tests/integration-test.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-set -o pipefail
+set -eo pipefail
 qemu_noise='^<jemalloc>:|^head:.*proc/self/exe|^ERROR: unknown platform bitness|^sh: [0-9]*: \[: Illegal number'
 
-DOCKER_BUILDKIT=1 docker build --platform linux/amd64 -t sysetup -f tests/Dockerfile . &&
-    docker run --platform linux/amd64 \
-        -e BW_CLIENTID="$(pw bitwarden_client_id)" \
-        -e BW_CLIENTSECRET="$(pw bitwarden_client_secret)" \
-        sysetup "$(pw bitwarden_master_password)" \
-        2>&1 | grep -Ev "$qemu_noise"
+BW_CLIENTID="${BW_CLIENTID:-$(pw bitwarden_client_id)}"
+BW_CLIENTSECRET="${BW_CLIENTSECRET:-$(pw bitwarden_client_secret)}"
+BITWARDEN="${BITWARDEN:-$(pw bitwarden_master_password)}"
+
+if [ -z "${IMAGE_TAG:-}" ]; then
+    IMAGE_TAG="sysetup"
+    DOCKER_BUILDKIT=1 docker build --platform linux/amd64 -t "$IMAGE_TAG" -f tests/Dockerfile .
+fi
+
+docker run --platform linux/amd64 \
+    -e BW_CLIENTID="$BW_CLIENTID" \
+    -e BW_CLIENTSECRET="$BW_CLIENTSECRET" \
+    "$IMAGE_TAG" "$BITWARDEN" \
+    2>&1 | grep -Ev "$qemu_noise"
