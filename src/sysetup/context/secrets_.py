e6d4c7f2380f4d9d754875b87b389a636b888c4f
Quinten Roets <62651391+quintenroets@users.noreply.github.com>
2026-06-22 20:53:50 -0700

    remove unused secret (#49)

--- src/sysetup/context/secrets_.py
+++ src/sysetup/context/secrets_.py
@@ -5,6 +5,3 @@ from dataclasses import dataclass, field
 @dataclass
 class Secrets:
     bw_clientid: str = field(default_factory=lambda: os.environ.get("BW_CLIENTID", ""))
-    bw_clientsecret: str = field(
-        default_factory=lambda: os.environ.get("BW_CLIENTSECRET", ""),
-    )
